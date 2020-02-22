199df41d37ff23cf5e317365feba65efedf384dc
Shubham Pandey <shubham>
2020-02-23 03:30:48 +0530

    sorting

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -7,11 +7,17 @@ import {
 } from "../actions/orderActions";
 
 import CardItem from "../components/CardItem";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
 import Grid from "@material-ui/core/Grid";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
 import Navbar from "../components/Navbar";
 import Pagination from "@material-ui/lab/Pagination";
+import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
 import { useDispatch } from "react-redux";
 import { useGoogleLogin } from "react-google-login";
 import { useGoogleLogout } from "react-google-login";
@@ -21,7 +27,7 @@ import { useSelector } from "react-redux";
 const useStyles = makeStyles(theme => ({
   body: {
     padding: theme.spacing(5),
-    paddingTop: theme.spacing(15),
+    paddingTop: theme.spacing(5),
     margin: "auto"
   },
   cardItem: {
@@ -32,6 +38,15 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     justifyContent: "center",
     padding: theme.spacing(5)
+  },
+  select: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: theme.spacing(10)
+  },
+  sortBy: {
+    display: "flex",
+    minWidth: theme.spacing(25)
   }
 }));
 
@@ -52,6 +67,7 @@ export default function DashboardPage() {
 
   const [searchVal, setSearchVal] = React.useState("");
   const [ordersState, setOrdersState] = React.useState([]);
+  const [sortBy, setSortBy] = React.useState("");
 
   //console.log("orders", orders);
 
@@ -64,6 +80,39 @@ export default function DashboardPage() {
     setOrdersState(orders);
     handleSearch(searchVal);
   }, [orders, searchVal]);
+
+  useEffect(() => {
+    console.log("sortBy changed", sortBy);
+    handleSort();
+  }, [sortBy]);
+
+  const handleSort = () => {
+    dispatch(getOrderPage(1));
+    setSearchVal("");
+    let toBeSortedArr = Object.assign({}, orders);
+    toBeSortedArr.payload = toBeSortedArr.payload.sort((valueA, valueB) => {
+      if (typeof valueA[sortBy] === "number") {
+        return valueA[sortBy] - valueB[sortBy];
+      } else if (sortBy === "date") {
+        return (
+          moment(valueA[sortBy], "DD/MM/YYYY").toDate() -
+          moment(valueB[sortBy], "DD/MM/YYYY").toDate()
+        );
+      } else {
+        let valA = valueA && valueA[sortBy] ? valueA[sortBy].toUpperCase() : "";
+        let valB = valueB && valueB[sortBy] ? valueB[sortBy].toUpperCase() : "";
+        if (valA < valB) {
+          return -1;
+        }
+        if (valA > valB) {
+          return 1;
+        }
+
+        return 0;
+      }
+    });
+    setOrdersState(toBeSortedArr);
+  };
 
   const handleSearch = searchVal => {
     let toBeSearched = String(searchVal);
@@ -90,6 +139,18 @@ export default function DashboardPage() {
   return (
     <div>
       <Navbar searchVal={searchVal} setSearchVal={setSearchVal} />
+      <Grid container className={classes.select}>
+        <FormControl variant="filled" className={classes.sortBy}>
+          <InputLabel>Sort By</InputLabel>
+          <Select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+            <MenuItem value={"title"}>Title</MenuItem>
+            <MenuItem value={"price"}>Price</MenuItem>
+            <MenuItem value={"seller"}>Seller</MenuItem>
+            <MenuItem value={"date"}>Date</MenuItem>
+            <MenuItem value={"rating"}>Rating</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
       <Grid container spacing={3} className={classes.body}>
         {ordersState &&
           ordersState.payload &&
@@ -110,7 +171,7 @@ export default function DashboardPage() {
               ? Math.ceil(ordersState.payload.length / 6)
               : 1
           }
-          page={ordersState.page}
+          page={orders.page}
           onChange={handleChange}
         />
       </Grid>
