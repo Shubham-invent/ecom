40dc9c1a559f20eb40e8696688c9628f382ce1a4
Shubham Pandey <shubham>
2020-02-23 14:21:02 +0530

    some test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,75 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { configure, shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Adapter from "enzyme-adapter-react-16";
+import CardItem from "./components/CardItem";
+import ListItem from "./components/ListItem";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import UserDetails from "./components/UserDetails";
+import ViewDetails from "./components/ViewDetails";
+import configureStore from "./store/configureStore";
+
+const store = configureStore({});
+
+configure({ adapter: new Adapter() });
+
+describe("<ViewDetails/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(<ViewDetails />);
+    expect(wrapper).toBeTruthy();
+  });
+});
+
+describe("<CardItem/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(
+      <CardItem
+        details={{
+          title: "hello",
+          price: 89,
+          seller: "",
+          date: "20/12/1993",
+          rating: 3,
+          imgUrl: ""
+        }}
+      />
+    );
+    expect(wrapper).toBeTruthy();
+  });
+});
+
+describe("<ListItem/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(<ListItem />);
+    expect(wrapper).toBeTruthy();
+  });
+});
+
+describe("<Navbar/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(<Navbar />);
+    expect(wrapper).toBeTruthy();
+  });
+});
+describe("<Sidebar/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Sidebar />
+      </Provider>
+    );
+    expect(wrapper).toBeTruthy();
+  });
+});
+describe("<UserDetails/>", () => {
+  it("renders", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <UserDetails />
+      </Provider>
+    );
+    expect(wrapper).toBeTruthy();
+  });
 });
--- src/index.stories.js
+++ src/index.stories.js
@@ -1,11 +1,38 @@
+import { refreshState, saveState } from "./store/localStorage";
+
 import Card from "./components/CardItem";
 import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import UserDetails from "./components/UserDetails";
+import configureStore from "./store/configureStore";
+
+const persistedstate = refreshState();
+const store = configureStore(persistedstate);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default { title: "Common Components" };
-export const NavbarComponent = () => <Navbar />;
-export const SidebarComponent = () => <Sidebar />;
-export const CardComponent = () => <Card />;
-export const UserDetailsComponent = () => <UserDetails />;
+export const NavbarComponent = () => (
+  <Provider store={store}>
+    <Navbar />
+  </Provider>
+);
+export const SidebarComponent = () => (
+  <Provider store={store}>
+    <Sidebar />
+  </Provider>
+);
+export const CardComponent = () => (
+  <Provider store={store}>
+    <Card />
+  </Provider>
+);
+export const UserDetailsComponent = () => (
+  <Provider store={store}>
+    <UserDetails />
+  </Provider>
+);
